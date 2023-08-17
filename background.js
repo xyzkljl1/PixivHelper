3f0de9b644229d80e42f23e001f766c50f10f2b1
xyzkljl1 <xyzkljl1@yahoo.com>
2023-08-18 03:54:03 +0800

    更改service worker keep alive的方法

--- background.js
+++ background.js
@@ -1,9 +1,5 @@
-let lifeline;
-console.log("start keep alive loop");
 SendCookie();
 setInterval(SendCookie, 12*60*60*1000);
-//防止service worker自己关闭
-keepAlive();
 
 function SendCookie() {
     //先访问一次刷新cookie(不知道有没有用)
@@ -26,38 +22,9 @@ function SendCookie() {
     });
 }
 
-chrome.runtime.onConnect.addListener(port => {
-    if (port.name === 'keepAlive') {
-        lifeline = port;
-        setTimeout(keepAliveForced, 25e3);  // 25sec，如果30秒没有新事件service worker就会关闭 #https://developer.chrome.com/blog/longer-esw-lifetimes/
-        port.onDisconnect.addListener(keepAliveForced);
-    }
-});
-
-function keepAliveForced() {
-    lifeline?.disconnect();
-    lifeline = null;
-    keepAlive();
-}
-
-async function keepAlive() {
-    if (lifeline) return;
-    for (const tab of await chrome.tabs.query({ url: '*://*/*' })) {
-        try {
-            await chrome.scripting.executeScript({
-                target: { tabId: tab.id },
-                function: () => chrome.runtime.connect({ name: 'keepAlive' }),
-                // `function` will become `func` in Chrome 93+
-            });
-            chrome.tabs.onUpdated.removeListener(retryOnTabUpdate);
-            return;
-        } catch (e) { }
-    }
-    chrome.tabs.onUpdated.addListener(retryOnTabUpdate);
-}
-
-async function retryOnTabUpdate(tabId, info, tab) {
-    if (info.url && /^(file|https?):/.test(info.url)) {
-        keepAlive();
-    }
-}
+console.log("Service Worker Start keep alive loop");
+// 25sec，如果30秒没有新事件service worker就会关闭 #https://developer.chrome.com/blog/longer-esw-lifetimes/
+// calling any asynchronous chrome API keeps the worker running for 30 seconds more #https://stackoverflow.com/questions/66618136/persistent-service-worker-in-chrome-extension
+const keepAlive = () => setInterval(chrome.runtime.getPlatformInfo, 25e3);
+chrome.runtime.onStartup.addListener(keepAlive);
+keepAlive();
